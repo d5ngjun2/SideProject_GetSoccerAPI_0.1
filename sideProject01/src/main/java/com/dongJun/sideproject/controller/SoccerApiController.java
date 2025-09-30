d7ee70cadd66936b64354cb42cffe086045f58a4
황동준 <nikihwangg@naver.com>
2025-09-30 16:02:06 +0900

    Backend Setting

---
+++ sideProject01/src/main/java/com/dongJun/sideproject/controller/SoccerApiController.java
@@ -0,0 +1,38 @@
+package com.dongJun.sideproject.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/soccer")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+public class SoccerApiController {
+
+    /*
+        api 링크
+        https://www.football-data.org/documentation/quickstart
+        토큰 키 : ccea26264fe842fb8f97e7940309faf3
+     */
+
+    // 리그 순위
+    @GetMapping("/standings")
+    public ResponseEntity<String> getStandingsApi() {
+        RestTemplate re = new RestTemplate();
+        RequestEntity<Void> req = RequestEntity
+                .get("http://api.football-data.org/v4/competitions/PL/standings")
+                .header("X-Auth-Token", "ccea26264fe842fb8f97e7940309faf3")
+                .build();
+
+        ResponseEntity<String> res = re.exchange(req, String.class);
+
+        return ResponseEntity.ok(res.getBody());
+    }
+}
---
+++ sideProject01/src/test/java/com/dongJun/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongJun.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
